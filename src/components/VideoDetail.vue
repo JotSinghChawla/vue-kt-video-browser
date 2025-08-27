7d9b5612a26a8ce2d011dc3836bbd692a5db16b1
Jot Singh <jot.chawla.54.js@gmail.com>
2025-08-28 02:58:18 +0530

    Competed video browser

---
+++ src/components/VideoDetail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="video" class="col-md-8">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" :src="videoUrl" />
+    </div>
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
+    </div>
+  </div>
+  <div v-else-if="videos_count > 0" class="col-md-8">
+    <div class="d-flex justify-content-center align-items-center h-100">
+      <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <h3 class="pl-2">Please select a video</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name:  'VideoDetail',
+  props: ['video', 'videos_count'],
+  computed: {
+    videoUrl() {
+      return `https://www.youtube.com/embed/${this.video.id.videoId}`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="list-group col-md-4">
+    <VideoListItem v-for="video_item in videos_arr" v-bind:key="video_item.etag" :video="video_item" @videoSelectedTemp="onVideoSelect">
+    </VideoListItem>
+      
+  </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem.vue';
+
+export default {
+  name: 'VideoList',
+  props: {
+    videos_arr: Array
+  },
+  components: {
+    VideoListItem
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.$emit('videoSelected', video)
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
